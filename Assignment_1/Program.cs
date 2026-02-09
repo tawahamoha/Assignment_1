3f155f149b838195fdc9819b9eed9981cdcd7d75
tawahamoha <tawahamoha@gmail.com>
2026-02-09 19:56:25 +0300

    the first assignment

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the invoice amount:");
+double amount = Convert.ToDouble(Console.ReadLine());
+
+
+if (amount <= 0)
+{
+    Console.WriteLine($"Invalid Amount: {amount}");
+}
+else if (amount >= 500)
+{
+    double discountAmount = (amount * 0.2);
+    double finalAmount = amount - discountAmount;
+    Console.WriteLine($"The Final Amount after the Discount (20%) is : {finalAmount}");
+}
+else if (amount < 500 && amount >= 300)
+{
+    double discountAmount = (amount * 0.1);
+    double finalAmount = amount - discountAmount;
+    Console.WriteLine($"The Final Amount after the Discount (10%) is : {finalAmount}");
+}
+else
+{
+    Console.WriteLine($"The Final Amount is: {amount}");
+}
