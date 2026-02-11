787fde0b731d9dd33030166304b50f774d33e9cb
tawahamoha <tawahamoha@gmail.com>
2026-02-11 15:49:57 +0300

    The Assignment

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.WriteLine("Enter The Amount of your Reciet :");
+double Amount  = Convert.ToDouble (Console.ReadLine());
+double discount_1 = 0.2;
+double discount_2 = 0.1;
+
+
+if (Amount < 0 )
+{
+    Console.WriteLine("Check your reciet again");
+}
+else if (Amount >= 500)
+{
+    Console.WriteLine($"Your Reciet After the Discount is :{Amount - (Amount * discount_1)}");
+}
+else if(Amount > 300 &&  Amount<500)
+{
+    Console.WriteLine($"Your Reciet After the Discount is :{Amount - (Amount * discount_2)}");
+
+}
+else if (Amount <=300)
+{
+    Console.WriteLine($"There is NO Discount on Your Reciet :{ Amount}");
+
+}
